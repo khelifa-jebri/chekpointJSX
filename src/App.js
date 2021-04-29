3adc5dade5c23bca5462fbc5f08805c77682135a
khelifa-jebri <khelifaisi@gmail.com>
2021-04-29 13:49:24 +0200

    adding_file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./style.css";
+import logo from "./zidan.jpg"
 
 function App() {
   return (
     <div className="App">
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+
+        <h1 className="title red">Your name here</h1>
+
+        <br/>
+
+        <img src={logo} alt="Zin Eddin Zidane" width="200"/>
+
+        <br/>
+
+        <img src="/maldini.jpg" alt="Palo Maldini" width="200"/>
+
+      </div>
+
+      <video width="320" height="240" controls>
+
+        <source src="/ES6.mp4" type="video/mp4" />
+
+      </video>
+      {/* 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +39,7 @@ function App() {
           Learn React
         </a>
       </header>
+      */}
     </div>
   );
 }
