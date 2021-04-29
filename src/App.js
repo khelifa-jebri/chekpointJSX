3453719839bde9b880905694db973a21bc6fc440
khelifa-jebri <khelifaisi@gmail.com>
2021-04-29 14:36:07 +0200

    Modification2

--- src/App.js
+++ src/App.js
@@ -24,22 +24,7 @@ function App() {
         <source src="/ES6.mp4" type="video/mp4" />
 
       </video>
-      {/* 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      */}
+    
     </div>
   );
 }
